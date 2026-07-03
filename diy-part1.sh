91027570696be87e475238fe9838a9718a06b401
mikanxx <1376106928@qq.com>
2026-07-03 10:57:46 +0800

    diy-part1: 添加QModem源

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,5 @@ echo >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
+# QModem
+echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
